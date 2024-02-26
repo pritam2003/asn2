742a4ff5610c52accfb6181cd5fd9f74e576982e
pritam2003 <85568503+pritam2003@users.noreply.github.com>
2024-02-25 20:27:42 -0800

    Update Student.java

--- src/main/java/pritam/asn2/models/Student.java
+++ src/main/java/pritam/asn2/models/Student.java
@@ -1,2 +1,80 @@
-package pritam.asn2.models;public class Student {
+package pritam.asn2.models;
+
+
+import jakarta.persistence.*;
+
+/**
+ * The model for a student. This class is used to create a table in the database.
+ *
+ * Nothing too fancy going on here. Just a bunch of getters and setters, and the constructors to create new students.
+ */
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sid; // student id
+    private String name;
+    private int weight;
+    private int height;
+    private String hairColor;
+    private float gpa;
+
+    public Student() {}
+
+    public Student(String name, int weight, int height, String hairColor, float gpa) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.gpa = gpa;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
 }
