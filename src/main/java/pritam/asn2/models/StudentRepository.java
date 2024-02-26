c7f48d39ef7fb025afea8acf154696453e99ea7f
pritam2003 <85568503+pritam2003@users.noreply.github.com>
2024-02-25 20:27:57 -0800

    Update StudentRepository.java

--- src/main/java/pritam/asn2/models/StudentRepository.java
+++ src/main/java/pritam/asn2/models/StudentRepository.java
@@ -1,2 +1,13 @@
-package pritam.asn2.models;public class StudentRepository {
+package pritam.asn2.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for the Student class. This interface is used to interact with the database, doing things like read and write.
+ *
+ * For this project, extending Spring Data JPA stuff is enough. By extending JpaRepository, we get a bunch of methods for free, such as findAll, findById, save, delete, etc.
+ *
+ * If any other special queries are needed, we can add them here.
+ */
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
